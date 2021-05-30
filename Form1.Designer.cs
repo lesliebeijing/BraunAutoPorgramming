94c2018a2aa0bc9926acc5c8e9a56f01a037bb1b
lesliefang <fl_beijing@126.com>
2021-05-30 18:00:36 +0800

    init project

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace BraunAutoProgramming
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelIp = new System.Windows.Forms.Label();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.labelPort = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtProposalList = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtResponse = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelIp
+            // 
+            this.labelIp.AutoSize = true;
+            this.labelIp.Location = new System.Drawing.Point(43, 13);
+            this.labelIp.Name = "labelIp";
+            this.labelIp.Size = new System.Drawing.Size(19, 17);
+            this.labelIp.TabIndex = 0;
+            this.labelIp.Text = "IP";
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(72, 10);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(136, 23);
+            this.txtIp.TabIndex = 1;
+            this.txtIp.Text = "192.168.100.41";
+            // 
+            // labelPort
+            // 
+            this.labelPort.AutoSize = true;
+            this.labelPort.Location = new System.Drawing.Point(30, 48);
+            this.labelPort.Name = "labelPort";
+            this.labelPort.Size = new System.Drawing.Size(32, 17);
+            this.labelPort.TabIndex = 2;
+            this.labelPort.Text = "Port";
+            // 
+            // txtPort
+            // 
+            this.txtPort.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.txtPort.Location = new System.Drawing.Point(72, 42);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 23);
+            this.txtPort.TabIndex = 3;
+            this.txtPort.Text = "4002";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "PropoalList Request";
+            // 
+            // txtProposalList
+            // 
+            this.txtProposalList.Location = new System.Drawing.Point(30, 107);
+            this.txtProposalList.Multiline = true;
+            this.txtProposalList.Name = "txtProposalList";
+            this.txtProposalList.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtProposalList.Size = new System.Drawing.Size(368, 371);
+            this.txtProposalList.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(422, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Response";
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.Location = new System.Drawing.Point(422, 107);
+            this.txtResponse.Multiline = true;
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResponse.Size = new System.Drawing.Size(361, 371);
+            this.txtResponse.TabIndex = 7;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(149, 510);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 8;
+            this.btnDownload.Text = "下发参数";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.CausesValidation = false;
+            this.label3.Location = new System.Drawing.Point(19, 553);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "author@fanglin";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 481);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(815, 579);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtResponse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtProposalList);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.labelPort);
+            this.Controls.Add(this.txtIp);
+            this.Controls.Add(this.labelIp);
+            this.Name = "Form1";
+            this.Text = "Braun autoprogramming ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelIp;
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.Label labelPort;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtProposalList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtResponse;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace BraunAutoProgramming
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string demoProposalXml = @"<ProposalList xmlns=""http://www.bbraun.com/HC/AIS/Space/AutoProgramming"">
+                                     <Drug>
+                                        <DrugId>201~0.25/50$3</DrugId>
+                                        <DrugName>Lasix 0.25/50</DrugName>
+                                        <DrugShort>0.005Lasix</DrugShort>
+                                        <InfusionRate>0.20</InfusionRate>
+                                        <RateUnit>ml/h</RateUnit>
+                                        <Checksum>62583</Checksum>
+                                     </Drug>
+                                     <Drug>
+                                       <DrugId>Insulin$4</DrugId>
+                                        <DrugName>Insulin</DrugName>
+                                        <InfusionRate>0.10</InfusionRate>
+                                        <RateUnit>ml/h</RateUnit>
+                                        <Checksum>52145</Checksum>
+                                     </Drug>
+                                    <ChecksumTotal>38110</ChecksumTotal>
+                                    </ProposalList>";
+            XElement root = XElement.Parse(demoProposalXml);
+            txtProposalList.Text = root.ToString();
+
+            label4.Text = "可下发一个或多个Drug到工作站,不用管Checksum可有可无,程序会自动处理Checksum";
+        }
+
+        private async void btnDownload_Click(object sender, EventArgs e)
+        {
+            string ip = txtIp.Text.Trim();
+            int.TryParse(txtPort.Text.Trim(), out int port);
+            if (ip.Length == 0 || port == 0)
+            {
+                return;
+            }
+
+            XNamespace aw = "http://www.bbraun.com/HC/AIS/Space/AutoProgramming";
+            XElement root = XElement.Parse(txtProposalList.Text);
+            IEnumerable<XElement> drugs = root.Elements(aw + "Drug");
+
+            string crcString = "";
+            foreach (var drug in drugs)
+            {
+                string s = "";
+                foreach (var item in drug.Elements())
+                {
+                    if (item.Name != aw + "Checksum")
+                    {
+                        s += (string)item;
+                    }
+                }
+                string crc = CRC16Util.calculate(s).ToString();
+                crcString += crc;
+
+                var checksumElement = drug.Element(aw + "Checksum");
+                if (checksumElement == null)
+                {
+                    drug.Add(new XElement(aw + "Checksum", crc));
+                }
+                else
+                {
+                    checksumElement.SetValue(crc);
+                }
+            }
+
+            string totalCrc = CRC16Util.calculate(crcString).ToString();
+            var checkSumTotal = root.Element(aw + "ChecksumTotal");
+            if (checkSumTotal == null)
+            {
+                root.Add(new XElement(aw + "ChecksumTotal", totalCrc));
+            }
+            else
+            {
+                checkSumTotal.SetValue(totalCrc);
+            }
+
+            txtProposalList.Text = root.ToString();
+
+            try
+            {
+                btnDownload.Enabled = false;
+                txtResponse.Text = "";
+                var downloadTask = Download2SpaceComAsync(ip, port, root.ToString());
+                var task = await Task.WhenAny(downloadTask, Task.Delay(5000));
+                if (task == downloadTask)
+                {
+                    string resp = await downloadTask;
+                    txtResponse.Text = resp;
+                }
+                else
+                {
+                    txtResponse.Text = "download timeout after 5 seconds";
+                    // downloadTask.ContinueWith(t => txtResponse.Text = t.Result);
+                }
+                btnDownload.Enabled = true;
+            }
+            catch (Exception e1)
+            {
+                txtResponse.Text = e1.ToString();
+                btnDownload.Enabled = true;
+            }
+        }
+
+        private async Task<string> Download2SpaceComAsync(String ip, int port, string proposalXml)
+        {
+            TcpClient tcpClient = new TcpClient();
+            await tcpClient.ConnectAsync(ip, port);
+            NetworkStream stream = tcpClient.GetStream();
+            StreamWriter streamWriter = new StreamWriter(stream);
+            StreamReader streamReader = new StreamReader(stream);
+            await streamWriter.WriteAsync(proposalXml);
+            string resp = await streamReader.ReadToEndAsync();
+            tcpClient.Close();
+            return resp;
+        }
+
+        /// <summary>
+        /// 原生 socket 实现,仅供参考
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <param name="port"></param>
+        /// <param name="proposalXml"></param>
+        /// <returns></returns>
+        private async Task<string> Download2SpaceComAsync2(String ip, int port, string proposalXml)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            await socket.ConnectAsync(IPAddress.Parse(ip), port);
+            await socket.SendAsync(Encoding.UTF8.GetBytes(proposalXml), SocketFlags.None);
+
+            byte[] buffer = new byte[1024 * 1024];
+            StringBuilder sb = new StringBuilder();
+            while (true)
+            {
+                int len = await socket.ReceiveAsync(buffer, SocketFlags.None);
+                string resp = Encoding.UTF8.GetString(buffer, 0, len);
+                sb.Append(resp);
+                if (len == -1)
+                {
+                    break;
+                }
+            }
+
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+
+            return sb.ToString();
+        }
+    }
+}
